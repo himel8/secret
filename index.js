d29d05d61e1585ad1031db00d10e2cc4d756fbc9
himel8 <himelkhan290@gmail.com>
2022-04-02 13:47:16 +0600

    port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
